3c009fdb18988560e85f9985cddd9e834c4ec6be
Ivan Shimko <vana.shimko@gmail.com>
2017-10-21 23:40:42 +0300

    Refactor

--- cubes/application.py
+++ cubes/application.py
@@ -20,43 +20,26 @@ class Application:
         glutInit(sys.argv)
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
         glutCreateWindow(b"Hello world!")
+
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_LIGHTING)
         glEnable(GL_LIGHT0)
-        
         glEnable(GL_COLOR_MATERIAL)
-        glutDisplayFunc(self.render)
-        glutSpecialFunc(self.on_key_press)
-        glutKeyboardFunc(self.on_key_press)
-        # glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE)
-        glMatrixMode(GL_PROJECTION)
-        # glScaled(5, 5, 5)
 
+        self.setup_callbacks()
 
     def render(self):
         glMatrixMode(GL_MODELVIEW)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         glLoadIdentity()
-        # glTranslate(0.7, 0, 0)
-        glRotatef(self._rotate_x, 1.0, 0.0, 0.0)
-        glRotatef(self._rotate_y, 0.0, 1.0, 0.0)
-        glRotatef(self._rotate_z, 0.0, 0.0, 1.0)
-        
 
-        glBegin(GL_QUADS)
-
-        for i, face in enumerate(scene_data.FACES):
-            glColor3f(*self._colors[i])
-            glNormal3f(*scene_data.NORMALS[i])
-            for point in face:
-                glVertex3f(point.x, point.y, point.z)
-
-        glEnd()
+        self.transformations()
+        self.draw_scene()
 
         glLoadIdentity()
 
-        glLight(GL_LIGHT0, GL_POSITION, [0, 0, -1, 0])
+        self.set_light()
 
         glFlush()
         glutSwapBuffers()
@@ -76,6 +59,27 @@ class Application:
             self._rotate_z -= 5
         glutPostRedisplay()
 
+    def draw_scene(self):
+        glBegin(GL_QUADS)
+        for i, face in enumerate(scene_data.FACES):
+            glColor3f(*self._colors[i])
+            glNormal3f(*scene_data.NORMALS[i])
+            for point in face:
+                glVertex3f(point.x, point.y, point.z)
+        glEnd()
+
+    def transformations(self):
+        glRotatef(self._rotate_x, 1.0, 0.0, 0.0)
+        glRotatef(self._rotate_y, 0.0, 1.0, 0.0)
+        glRotatef(self._rotate_z, 0.0, 0.0, 1.0)
+
+    def set_light(self):
+        glLight(GL_LIGHT0, GL_POSITION, [0, 0, -1, 0])
+
+    def setup_callbacks(self):
+        glutDisplayFunc(self.render)
+        glutSpecialFunc(self.on_key_press)
+        glutKeyboardFunc(self.on_key_press)
+
     def start(self):
         glutMainLoop()
-
