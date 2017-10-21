b56597562de000f4def884d28824283822516989
Ivan Shimko <vana.shimko@gmail.com>
2017-10-21 22:56:25 +0300

    Add fixed light

--- cubes/application.py
+++ cubes/application.py
@@ -13,27 +13,68 @@ class Application:
         self._colors = [(random(), random(), random()) for _ in scene_data.FACES]
         self._rotate_y = 0
         self._rotate_x = 0
+        self._rotate_z = 0
 
         glutInit(sys.argv)
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
         glutCreateWindow(b"Hello world!")
         glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        
+        glEnable(GL_COLOR_MATERIAL)
         glutDisplayFunc(self.render)
         glutSpecialFunc(self.on_key_press)
+        glutKeyboardFunc(self.on_key_press)
+        # glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE)
+        glMatrixMode(GL_PROJECTION)
+        # glScaled(5, 5, 5)
+
 
     def render(self):
+        glMatrixMode(GL_MODELVIEW)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
         glLoadIdentity()
+        # glTranslate(0.7, 0, 0)
         glRotatef(self._rotate_x, 1.0, 0.0, 0.0)
         glRotatef(self._rotate_y, 0.0, 1.0, 0.0)
+        glRotatef(self._rotate_z, 0.0, 0.0, 1.0)
+        
+
         glBegin(GL_QUADS)
 
         for i, face in enumerate(scene_data.FACES):
             glColor3f(*self._colors[i])
+            coordinates_last = [None, None, None]
+            coordinates_status = [True, True, True]
+            for point in face:
+                if coordinates_last[0] is None:
+                    coordinates_last[0] = point.x
+                elif coordinates_status[0] and coordinates_last[0] != point.x:
+                    coordinates_status[0] = False
+
+                if coordinates_last[1] is None:
+                    coordinates_last[1] = point.y
+                elif coordinates_status[1] and coordinates_last[1] != point.y:
+                    coordinates_status[1] = False
+
+                if coordinates_last[2] is None:
+                    coordinates_last[2] = point.z
+                elif coordinates_status[2] and coordinates_last[2] != point.z:
+                    coordinates_status[2] = False
+
+            normal = [-int(x) for x in coordinates_status]
+            glNormal3f(*scene_data.NORMALS[i])
             for point in face:
                 glVertex3f(point.x, point.y, point.z)
 
         glEnd()
+
+        glLoadIdentity()
+
+        glLight(GL_LIGHT0, GL_POSITION, [0, 0, -1, 0])
+
         glFlush()
         glutSwapBuffers()
 
@@ -46,6 +87,10 @@ class Application:
             self._rotate_x += 5
         elif key == GLUT_KEY_DOWN:
             self._rotate_x -= 5
+        elif key == b'z':
+            self._rotate_z += 5
+        elif key == b'x':
+            self._rotate_z -= 5
         glutPostRedisplay()
 
     def start(self):
