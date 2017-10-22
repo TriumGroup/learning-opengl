1bf04fc3447f809cb6774b27be02de420925e994
Ivan Shimko <vana.shimko@gmail.com>
2017-10-22 11:37:17 +0300

    Add changing material properties

--- cubes/application.py
+++ cubes/application.py
@@ -7,6 +7,8 @@ from OpenGL.GLUT import *
 
 import cubes.scene_data as scene_data
 
+Material = namedtuple('Material', ['ambient', 'diffuse', 'specular', 'shine'])
+
 class Rotation:
     def __init__(self):
         self.x = 0
@@ -23,6 +25,34 @@ class Application:
         b'z' : ('z', +1),
         b'x' : ('z', -1)
     }
+    _COLOR_MATERIAL = -1
+    _MATERIALS = [
+        _COLOR_MATERIAL,
+        Material(
+            ambient=[0.329412, 0.223529, 0.027451, 1.0],
+            diffuse=[0.780392, 0.568627, 0.113725, 1.0],
+            specular=[0.992157, 0.941176, 0.807843, 1.0],
+            shine=27.8974
+        ),
+        Material(
+            ambient=[0.2125, 0.1275, 0.054, 1.0],
+            diffuse=[0.714, 0.4284, 0.18144, 1.0],
+            specular=[0.393548, 0.271906, 0.166721, 1.0],
+            shine=25.6
+        ),
+        Material(
+            ambient=[0.25, 0.25, 0.25, 1.0],
+            diffuse=[0.4, 0.4, 0.4, 1.0],
+            specular=[0.774597, 0.774597, 0.774597, 1.0],
+            shine=76.8
+        ),
+        Material(
+            ambient=[0.05375, 0.05, 0.06625, 0.82],
+            diffuse=[0.18275, 0.17, 0.22525, 0.82],
+            specular=[0.332741, 0.328634, 0.346435, 0.82],
+            shine=38.4
+        )
+    ]
 
     def __init__(self):
         self._colors = [(random(), random(), random()) for _ in scene_data.FACES]
@@ -32,13 +62,13 @@ class Application:
         self._current_rotation = 0
         self._lightning = True
         self._spot_light = True
+        self._material = 0
 
         glutInit(sys.argv)
         glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
         glutCreateWindow(b"Hello world!")
 
         glEnable(GL_DEPTH_TEST)
-        glEnable(GL_COLOR_MATERIAL)
         glEnable(GL_LIGHTING)
 
         self.setup_callbacks()
@@ -58,6 +88,7 @@ class Application:
         glLoadIdentity()
 
         self.scene_transformations()
+        self.set_material()
         self.draw_scene()
 
         glFlush()
@@ -77,7 +108,8 @@ class Application:
             self._lightning = not self._lightning
         elif key == b'd':
             self._spot_light = not self._spot_light
-        glutPostRedisplay()
+        elif key == b'm':
+            self._material = (self._material + 1) % len(self._MATERIALS)
         glutPostRedisplay()
 
     def draw_scene(self):
@@ -103,16 +135,31 @@ class Application:
     def draw_light(self):
         glLightfv(GL_LIGHT0, GL_POSITION, (0, 0, -1, int(self._spot_light)))
 
-    def setup_light(self):
-        if self._lightning:
-            glEnable(GL_LIGHT0)
+    def set_material(self):
+        material = self._MATERIALS[self._material]
+        if material == self._COLOR_MATERIAL:
+            glEnable(GL_COLOR_MATERIAL)
         else:
-            glDisable(GL_LIGHT0)
+            glDisable(GL_COLOR_MATERIAL)
+            glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material.ambient)
+            glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material.diffuse)
+            glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, material.specular)
+            glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, material.shine)
+
+    def setup_light(self):
+        self.gl_set_bool(GL_LIGHT0, self._lightning)
 
     def setup_callbacks(self):
         glutDisplayFunc(self.render)
         glutSpecialFunc(self.on_key_press)
         glutKeyboardFunc(self.on_key_press)
+
+    @staticmethod
+    def gl_set_bool(name, value):
+        if value:
+            glEnable(name)
+        else:
+            glDisable(name)
 
     def start(self):
         glutMainLoop()
